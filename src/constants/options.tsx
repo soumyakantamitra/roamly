e2cfcec19f4f791af60e5401e83f7ea1f19447a5
soumyakantamitra <soumyakantamitra89@gmail.com>
2025-06-18 23:07:27 +0530

    creating the user form

---
+++ src/constants/options.tsx
@@ -0,0 +1,51 @@
+export const travelerCount= [
+  {
+    id: 1,
+    title: 'Only Me',
+    desc: 'One travels alone in pursuit of exploration',
+    icon: '✈️',
+    people: '1 people'
+  },
+  {
+    id: 2,
+    title: 'Couple',
+    desc: 'Two voyagers on a shared path',
+    icon: '🌄',
+    people: '1 people'
+  },
+  {
+    id: 3,
+    title: 'Family',
+    desc: 'A fun-loving family always ready for adventure',
+    icon: '🏡',
+    people: '3 to 5 people'
+  },
+  {
+    id: 4,
+    title: 'Friends',
+    desc: 'A tight group of pals always up for the next big thrill',
+    icon: '🤘',
+    people: '5 to 10 people'
+  }
+]
+
+export const budgetSelect = [
+  {
+    id: 1,
+    title: 'Affordable',
+    desc: 'Cost conscious',
+    icon: '💵',
+  },
+  {
+    id: 2,
+    title: 'Moderate',
+    desc: 'Average costs',
+    icon: '💳'
+  },
+  {
+    id: 3,
+    title: 'Luxurious',
+    desc: 'Costs dont matter',
+    icon: '💎'
+  }
+]
--- src/create-trip/CreateTrip.tsx
+++ src/create-trip/CreateTrip.tsx
@@ -1,14 +1,31 @@
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+
 function CreateTrip() {
+
+  const [place, setPlace] = useState("");
+
+  const handleChange = (value: string) => {
+    setPlace(value);
+  }
+
   return (
-    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
       <h2 className="font-bold text-3xl">Choose your travel preferences</h2>
       <p className="mt-3 text-gray-500 text-xl">
         Let us know what you’re looking for in a trip, and our planner will design an itinerary you’ll love.
       </p>
-      <div>
-        <h2 className="text-xl">Enter your destination of choice?</h2>
-
+      <div className="mt-20 flex flex-col gap-10">
+        <div>
+          <h2 className="text-xl my-3 font-medium">Enter your destination of choice</h2>
+          <Input type="text" placeholder="Location" value={place} onChange={(e) => handleChange(e.target.value)}/>
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">Let us know the number of days you plan to travel</h2>
+          <Input type="number" placeholder="e.g. 12" value={place} onChange={(e) => handleChange(e.target.value)}/>
+        </div>
       </div>
+      
     </div>
   )
 }
