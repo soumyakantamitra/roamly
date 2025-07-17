7bffba28c1c99473c26bd0f1202721bd0c5bd8e3
soumyakantamitra <soumyakantamitra89@gmail.com>
2025-07-17 22:26:49 +0530

    fix: removed minor unnecessary code

--- src/view-trip/components/Hotels.tsx
+++ src/view-trip/components/Hotels.tsx
@@ -6,9 +6,9 @@ function Hotels({ trip } : TripType) {
     <div>
       <h2 className="font-bold text-xl mt-10">Hotel Recommendations</h2>
       <div className="my-5 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
-        {trip?.tripData?.hotel_options.map((hotel, index) => (
+        {trip?.tripData?.hotel_options.map((hotel) => (
           <Link to={'https://www.google.com/maps/search/?api=1&query='+ hotel?.name + ", " + hotel?.address} target="_blank">
-            <div key={index} className="hover:scale-105 transition duration-300">
+            <div className="hover:scale-105 transition duration-300">
               <img src="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dHJhdmVsfGVufDB8fDB8fHww" alt="" 
                 className="rounded-xl"
               />
