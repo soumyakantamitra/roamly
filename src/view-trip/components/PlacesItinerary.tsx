7297b95297943b03d6cb877c95a43e9c904da4b7
soumyakantamitra <soumyakantamitra89@gmail.com>
2025-07-20 22:31:42 +0530

    feat: added separate componet for each day plans

--- src/view-trip/components/PlacesItinerary.tsx
+++ src/view-trip/components/PlacesItinerary.tsx
@@ -1,4 +1,5 @@
 import type { TripType } from "../types"
+import PlacesItem from "./PlacesItem"
 
 function PlacesItinerary( {trip}: TripType ) {
   return (
@@ -8,13 +9,15 @@ function PlacesItinerary( {trip}: TripType ) {
       <div>
         {trip.tripData?.itinerary.map((plan) => (
           <div>
-            <h2 className="font-medium text-lg">Day {plan.day}</h2>
-            {plan.places.map((venue) => (
-              <div>
-                <h2>{venue.name}</h2>
-                <h2 className="text-sm font-medium text-blue-600">{venue.best_time_to_visit}</h2>
-              </div>
-            ))}
+            <h2 className="font-medium text-lg mt-5">Day {plan.day}</h2>
+            <div className="grid md:grid-cols-2 gap-5">
+              {plan.places.map((venue) => (
+                <div className="my-3">
+                  <h2 className="text-sm font-medium text-blue-600">{venue.best_time_to_visit}</h2>
+                  <PlacesItem place={venue} />
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
