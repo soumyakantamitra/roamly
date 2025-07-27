dc97c4b849dbc5afaab376c7b28c6b8e5570cfb1
soumyakantamitra <soumyakantamitra89@gmail.com>
2025-07-27 22:19:48 +0530

    feat: added caching of images

--- src/service/ImageApi.tsx
+++ src/service/ImageApi.tsx
@@ -9,12 +9,41 @@ const config = {
   }
 }
 
+const getFromLocalStorage = (key: string) => {
+  const storage = localStorage.getItem("cachedImages");
+  if (storage) {
+    const cachedStorage = JSON.parse(storage);
+    if (key in cachedStorage) {
+      return cachedStorage[key];
+    }
+  }
+  return false;
+};
+
+const setToLocalStorage = (key: string, value: string) => {
+  const storage = localStorage.getItem("cachedImage");
+  if (storage) {
+    const cachedStorage = JSON.parse(storage);
+    cachedStorage[key] = value;
+  }
+  else {
+    localStorage.setItem("cachedImages", JSON.stringify({[key] : value}));
+  }
+
+};
+
 export default async function getLocationImage(data: string) {
+
+  const cachedImage = getFromLocalStorage(data);
+  if (cachedImage) {
+    return cachedImage;
+  }
 
   const apiUrl = baseApi + data;
   try {
     const response = await axios.get(apiUrl, config);
     console.log(response.data.photos[0].src.original);
+    setToLocalStorage(data, response.data.photos[0].src.original);
     return response.data.photos[0].src.original;
   } 
   catch (error) {
