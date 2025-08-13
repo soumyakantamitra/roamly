f0a998c49aefea82cfea6f2eb38cfdfc893fbc0e
soumyakantamitra <soumyakantamitra89@gmail.com>
2025-08-13 20:55:36 +0530

    feat: added a background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import './App.css'
-import Hero from './components/custom/Hero'
+import "./App.css";
+import Hero from "./components/custom/Hero";
+import { useTheme } from "./components/theme-provider";
 
 function App() {
+  const { theme } = useTheme();
 
   return (
     <>
-      {/* Hero */}
-      <Hero />
+      <div className="min-h-screen w-full relative pt-16">
+        <div
+          className="absolute inset-0 z-0"
+          style={
+            theme === "light"
+              ? {
+                  background:
+                    "radial-gradient(125% 125% at 50% 10%, #fff 40%, #6366f1 100%)",
+                }
+              : {
+                  background: "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(120, 180, 255, 0.25), transparent 70%), #000000",
+                }
+          }
+        />
+        <div className="relative ">
+          <Hero />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
